d56ef65a11305293f0cca9895114531b06f85bea
Ngqentsu <122878446+Ngqentsu@users.noreply.github.com>
2024-07-24 20:40:47 +0200

    Delete 0x01-ES6_promise directory

--- 0x01-ES6_promise/0-promise.js
+++
@@ -1,9 +0,0 @@
-export function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('API response received');
-    }, 1000);
-  });
-}
-
-export default getResponseFromAPI;
